8b070ce13812b4d56e624fd9fd2c23313187934f
Dylan Valentin <dylan.valentin@ik.me>
2026-01-11 16:09:04 +0100

    feat(Proto3Processor): message field extensions

--- src/proto3_processor/classes/proto3_field_processor.ts
+++ src/proto3_processor/classes/proto3_field_processor.ts
@@ -1,10 +1,13 @@
 import { fileContentMatter } from '#file/classes/content_matter'
 import type { FreshFileContentMatter } from '#file/types/content_matter'
+import { Proto3Extension } from '#proto3_definition/types/extension'
 import type {
     AnyProto3Field,
     Proto3MessageFieldType,
 } from '#proto3_definition/types/fields'
+import { Proto3ImportedType } from '#proto3_definition/types/imported_type'
 import type { Proto3PrimitifType } from '#proto3_definition/types/primitifs'
+import { Proto3ExtensionValueProcessor } from '#proto3_processor/classes/proto3_extension_value_processor'
 import { match } from 'ts-pattern'
 
 export class Proto3FieldProcessor {
@@ -70,14 +73,82 @@ export class Proto3FieldProcessor {
         return finalType
     }
 
+    private getExtensionContent(extension: Proto3Extension): FreshFileContentMatter {
+        const extensionContent = fileContentMatter()
+
+        const extensionValueContent = fileContentMatter()
+        const processor = new Proto3ExtensionValueProcessor(extensionValueContent)
+
+        processor.process(extension.value)
+
+        extensionContent
+            .write(`${extension.key.typeReference} = `)
+            .write(extensionValueContent)
+
+        return extensionContent
+    }
+
+    private shouldSkipExtensionDependingOnValue(
+        extensionValue: Proto3Extension['value']
+    ): boolean {
+        if (typeof extensionValue === 'object' && !Array.isArray(extensionValue)) {
+            const messageEntryLength = Object.keys(extensionValue).length
+
+            if (messageEntryLength === 0) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private alterateExtensionDependingOnValue(
+        extension: Proto3Extension
+    ): Proto3Extension {
+        if (typeof extension.value !== 'object' || Array.isArray(extension.value)) {
+            return extension
+        }
+
+        const messageEntries = Object.entries(extension.value)
+
+        if (messageEntries.length < 1 || messageEntries.length > 1) {
+            return extension
+        }
+
+        const firstKey = messageEntries[0]![0]
+        const firstValue = messageEntries[0]![1]
+
+        return Proto3Extension.new({
+            ...extension,
+            key: Proto3ImportedType.new({
+                ...extension.key,
+                typeReference: `(${extension.key.typeReference}).${firstKey}`,
+            }),
+            value: firstValue,
+        })
+    }
+
     public process(anyField: AnyProto3Field): void {
         match(anyField)
             .with({ internalName: 'message_field' }, (field) => {
-                // TODO: run extension => puis remplacer EXTs
-                // message LogInInput {
-                //     map<string, double> test444 = 1 [(buf.validate.field).required = true];
-                // }
-                // field.extensions
+                const extensionContents: FreshFileContentMatter[] = []
+
+                for (const extension of field.extensions) {
+                    const shouldSkip = this.shouldSkipExtensionDependingOnValue(
+                        extension.value
+                    )
+
+                    if (shouldSkip) {
+                        continue
+                    }
+
+                    const updatedExtension =
+                        this.alterateExtensionDependingOnValue(extension)
+
+                    const extensionContent = this.getExtensionContent(updatedExtension)
+
+                    extensionContents.push(extensionContent)
+                }
 
                 const typeString = this.getTypeReferenceString(field.type)
 
@@ -85,9 +156,13 @@ export class Proto3FieldProcessor {
                     this.fileContent.write('optional ')
                 }
 
-                this.fileContent.write(
-                    `${typeString} ${field.key} = ${field.index} EXTs;`
-                )
+                this.fileContent.write(`${typeString} ${field.key} = ${field.index}`)
+
+                if (extensionContents.length > 0) {
+                    this.fileContent.write(' ').writeList(...extensionContents)
+                }
+
+                this.fileContent.write(';')
             })
             .with({ internalName: 'message_one_of_field' }, (field) => {
                 const oneOfFieldContent = fileContentMatter()
