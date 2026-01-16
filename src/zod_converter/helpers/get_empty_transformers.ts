a81b6eee5f0fbf3d7c03429c09586708a57c5899
Dylan Valentin <dylan.valentin@ik.me>
2026-01-16 17:47:07 +0100

    fix(GetEmptyTransformers): missing transformer arrays

--- src/zod_converter/helpers/get_empty_transformers.ts
+++ src/zod_converter/helpers/get_empty_transformers.ts
@@ -2,6 +2,9 @@ import { ZodConversionTransformers } from '#zod_converter/types/transformers'
 
 export const getEmptyTranformers = function (): ZodConversionTransformers {
     return {
+        message: [],
         messageField: [],
+        messageOneOfField: [],
+        enum: [],
     }
 }
