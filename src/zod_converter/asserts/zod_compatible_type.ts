8c7fcf294e0ca7f5fddd314e753c496f8ed7a5eb
Dylan Valentin <dylan.valentin@ik.me>
2026-01-18 17:29:21 +0100

    fix(ZodConverter): assertsZodCompatibleType type

--- src/zod_converter/asserts/zod_compatible_type.ts
+++ src/zod_converter/asserts/zod_compatible_type.ts
@@ -1,18 +1,13 @@
-import { CompatibleZodTypeTuple, ZodCompatibleType } from '#zod_converter/types/check2'
+import { CompatibleZodTypeTuple, ZodCompatibleType } from '#zod_converter/types/check'
 import { SchemaError } from '#zod_converter/types/error'
 import type { SomeType } from 'zod/v4/core'
 
-type AssertsZodCompatibleTypeFn = <TSchema extends SomeType>(
-    schema: TSchema
-    // @ts-expect-error fuck of
+type AssertsZodCompatibleTypeFn = (
+    schema: SomeType
 ) => asserts schema is ZodCompatibleType
 
-export const assertsZodCompatibleType: AssertsZodCompatibleTypeFn = function <
-    TSchema extends SomeType,
->(
-    schema: TSchema
-    // @ts-expect-error fuck of
-    // faire un SomeCompatibleType
+export const assertsZodCompatibleType: AssertsZodCompatibleTypeFn = function (
+    schema: SomeType
 ): asserts schema is ZodCompatibleType {
     if (!ZodCompatibleType.is(schema)) {
         const validZodTypes = CompatibleZodTypeTuple.get()
